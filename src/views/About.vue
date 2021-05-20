58a9843ec19eec579fc031945ead6824fe23ff78
mwk <2515011987@qq.com>
2021-05-20 17:25:10 +0800

    配置路由及项目所用ui框架

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div></div>
 </template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="less"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="../../static/download/111.txt" download="test">测试下载</a>
+    <el-button type="success" class="to_lead" @click.native="exportData()">
+      <span class="glyphicon glyphicon-arrow-up"></span> 导出
+    </el-button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" />使用公共组件 -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";//导入公共组件
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld//注册公共组件
+  },
+  data() {},
+  methods: {
+    exportData() {
+      location.href = "../../static/download/111.txt";
+    }
   }
-}
+};
 </script>
